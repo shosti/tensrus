9bba25d4384209f459c802992550a13a45191eb5
Emanuel Evans <me@eevans.co>
2024-04-18 09:13:39 -0700

    Implement hashing for values

--- src/value.rs
+++ src/value.rs
@@ -1,7 +1,10 @@
 use crate::numeric::Numeric;
+use rand::random;
 use std::collections::hash_set::HashSet;
+use std::hash::{Hash, Hasher};
 
 pub struct Value<T: Numeric> {
+    id: u64,
     data: T,
     grad: f64,
     backward: Option<Box<dyn FnMut() -> ()>>,
@@ -12,6 +15,7 @@ pub struct Value<T: Numeric> {
 impl<T: Numeric> Value<T> {
     pub fn new(val: T) -> Self {
         Self {
+            id: random(),
             data: val,
             grad: 0.0,
             backward: None,
@@ -21,12 +25,26 @@ impl<T: Numeric> Value<T> {
     }
 }
 
+impl<T: Numeric> PartialEq for Value<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
+impl<T: Numeric> Eq for Value<T> {}
+
+impl<T: Numeric> Hash for Value<T> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.id.hash(state);
+    }
+}
+
 impl<T: Numeric> std::fmt::Debug for Value<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "Value(data={}, grad={}, op={})",
-            self.data, self.grad, self.op
+            "Value(id={}, data={}, grad={}, op={})",
+            self.id, self.data, self.grad, self.op
         )
     }
 }
