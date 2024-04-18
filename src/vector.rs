fca4d1e950a688d5f541de07b02c785b8a93c983
Emanuel Evans <me@eevans.co>
2024-04-17 23:10:19 -0700

    Pedantic row vector stuff

--- src/vector.rs
+++ src/vector.rs
@@ -22,14 +22,14 @@ impl<T: Num + Copy, const N: usize> Vector<T, N> {
 
 impl<T: Num + Copy, const N: usize> From<[T; N]> for Vector<T, N> {
     fn from(vals: [T; N]) -> Self {
-        Vector {
+        Self {
             vals: Rc::new(RefCell::new(Vec::from(vals))),
         }
     }
 }
 
 impl<T: Num + Copy, const N: usize> Tensor<T, 1> for Vector<T, N> {
-    type Transpose = Vector<T, N>;
+    type Transpose = RowVector<T, N>;
 
     fn from_fn<F>(mut cb: F) -> Self
     where
@@ -86,6 +86,74 @@ impl<T: Num + Copy, const N: usize> PartialEq for Vector<T, N> {
 
 impl<T: Num + Copy, const N: usize> Eq for Vector<T, N> {}
 
+#[derive(Debug)]
+pub struct RowVector<T: Num + Copy, const N: usize> {
+    vals: Rc<RefCell<Vec<T>>>,
+}
+
+impl<T: Num + Copy, const N: usize> From<[T; N]> for RowVector<T, N> {
+    fn from(vals: [T; N]) -> Self {
+        Vector::from(vals).transpose()
+    }
+}
+
+impl<T: Num + Copy, const N: usize> Tensor<T, 1> for RowVector<T, N> {
+    type Transpose = Vector<T, N>;
+
+    fn from_fn<F>(cb: F) -> Self
+    where
+        F: FnMut([usize; 1]) -> T,
+    {
+        Vector::from_fn(cb).transpose()
+    }
+
+    fn shape(&self) -> [usize; 1] {
+        [N]
+    }
+
+    fn get(&self, idx: [usize; 1]) -> Result<T, IndexError> {
+        let [i] = idx;
+        if i >= N {
+            return Err(IndexError {});
+        }
+
+        Ok(self.vals.borrow()[i])
+    }
+
+    fn set(&mut self, idx: [usize; 1], val: T) -> Result<(), IndexError> {
+        let [i] = idx;
+        if i >= N {
+            return Err(IndexError {});
+        }
+        self.vals.borrow_mut()[i] = val;
+
+        Ok(())
+    }
+
+    fn transpose(&self) -> Self::Transpose {
+        Self::Transpose {
+            vals: self.vals.clone(),
+        }
+    }
+}
+
+impl<T: Num + Copy, const N: usize> MulAssign<T> for RowVector<T, N> {
+    fn mul_assign(&mut self, other: T) {
+        self.vals
+            .borrow_mut()
+            .iter_mut()
+            .for_each(|n| *n = *n * other);
+    }
+}
+
+impl<T: Num + Copy, const N: usize> PartialEq for RowVector<T, N> {
+    fn eq(&self, other: &Self) -> bool {
+        (0..N).all(|i| self.get([i]) == other.get([i]))
+    }
+}
+
+impl<T: Num + Copy, const N: usize> Eq for RowVector<T, N> {}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -112,5 +180,16 @@ mod tests {
         let b = Vector::from([4, 5, 6]);
 
         assert_eq!(a.dot(&b), 32);
+    }
+
+    #[test]
+    fn transpose() {
+        let x = Vector::from([1, 2, 3]);
+
+        assert_eq!(x.transpose(), RowVector::from([1, 2, 3]));
+    }
+
+    #[test]
+    fn row_vec_matrix_mul() {
     }
 }
