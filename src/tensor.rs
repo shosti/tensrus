98a7d262133ffa6635984b2793c480c1bd13f3a1
Emanuel Evans <me@eevans.co>
2024-04-21 11:50:34 -0700

    Delete some dead code

--- src/tensor.rs
+++ src/tensor.rs
@@ -153,41 +153,6 @@ impl<T: Numeric, const R: usize, const S: TensorShape> PartialEq for Tensor<T, R
 
 impl<T: Numeric, const R: usize, const S: TensorShape> Eq for Tensor<T, R, S> {}
 
-// pub trait Tensor<T: Numeric, const R: usize>: MulAssign<T> + Eq {
-//     type Transpose;
-
-//     fn from_fn<F>(cb: F) -> Self
-//     where
-//         F: FnMut([usize; R]) -> T;
-//     fn zeros() -> Self
-//     where
-//         Self: Sized,
-//     {
-//         Self::from_fn(|_| T::zero())
-//     }
-
-//     fn rank(&self) -> usize {
-//         R
-//     }
-//     fn shape(&self) -> [usize; R];
-//     fn get(&self, idx: [usize; R]) -> Result<T, IndexError>;
-//     fn set(&mut self, idx: [usize; R], val: T) -> Result<(), IndexError>;
-//     fn transpose(&self) -> Self::Transpose;
-//     fn next_idx(&self, idx: [usize; R]) -> Option<[usize; R]>;
-
-//     fn update<F>(&mut self, mut cb: F)
-//     where
-//         F: FnMut(T) -> T,
-//     {
-//         let mut iter = Some([0; R]);
-//         while let Some(idx) = iter {
-//             let cur = self.get(idx).unwrap();
-//             self.set(idx, cb(cur)).unwrap();
-//             iter = self.next_idx(idx);
-//         }
-//     }
-// }
-
 #[cfg(test)]
 mod tests {
     use super::*;
