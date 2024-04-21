bf16ef7b925c7de5fc4f8774b2f4dd739654e159
Emanuel Evans <me@eevans.co>
2024-04-21 07:30:44 -0700

    More tests passing!

--- tests/value_test.rs
+++ tests/value_test.rs
@@ -19,6 +19,29 @@ fn value_sanity_test() {
     assert_eq!(x.grad(), 46.0);
 }
 
+#[test]
+fn test_more_ops() {
+    let a = Value::from(-4.0);
+    let b = Value::from(2.0);
+    let mut c = a.clone() + b.clone();
+    let mut d = a.clone() * b.clone() + b.clone().pow(3.0);
+    c = c.clone() + (c.clone() + Value::from(1.0));
+    c = c.clone() + (Value::from(1.0) + c.clone() + (-a.clone()));
+    d = d.clone() + (d.clone() * Value::from(2.0) + (b.clone() + a.clone()).relu());
+    d = d.clone() + (Value::from(3.0) * d.clone() + (b.clone() - a.clone()).relu());
+    let e = c.clone() - d.clone();
+    let f = e.pow(2.0);
+    let mut g: Value<f64> = f.clone() / Value::from(2.0);
+    g = g.clone() + (Value::from(10.0) / f.clone());
+    g.backward();
+
+    let tol = 0.00000001;
+
+    assert!((g.val() - 24.70408163265306).abs() < tol);
+    assert!((a.grad() - 138.83381924198252).abs() < tol);
+    assert!((b.grad() - 645.5772594752187).abs() < tol);
+}
+
 fn render_graph(x: Value<f64>, id: String) {
     let g = Graph::new(x.clone());
     let dotfile = format!("/tmp/{}.dot", id);
