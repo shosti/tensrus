927914794ccc0ffae294374de0f9ae3df9001e42
Emanuel Evans <me@eevans.co>
2024-06-01 09:28:52 -0700

    Moving into different models

--- makemore-1/src/main.rs
+++ makemore-1/src/main.rs
@@ -7,29 +7,32 @@ const BOUNDARY: char = '.';
 
 fn main() {
     let names = read_names();
-    let prob_matrix = get_bigrams(&names).map(|_, n| n + 1.0).normalize_rows();
-    let mut log_likelihood = 0.0;
-    let mut n = 0;
-    for name in &["andrejq".to_string()] {
-        let mut cur = BOUNDARY;
-        let mut chars = name.chars().chain(vec![BOUNDARY].into_iter()).peekable();
-        while let Some(next) = chars.peek() {
-            let prob = prob_matrix[&[stoi(cur), stoi(*next)]];
-            let logprob = prob.ln();
-            log_likelihood += logprob;
-            n += 1;
-            cur = chars.next().unwrap();
-        }
-    }
-    let nll = -log_likelihood;
-    println!("{:.4}", nll / n as f32);
+    let (ins, outs) = get_training_set(&names[..10]);
+    println!("INS: {:?}", ins);
+    println!("OUTS: {:?}", outs);
+    // let prob_matrix = get_bigrams(&names).map(|_, n| n + 1.0).normalize_rows();
+    // let mut log_likelihood = 0.0;
+    // let mut n = 0;
+    // for name in &["andrejq".to_string()] {
+    //     let mut cur = BOUNDARY;
+    //     let mut chars = name.chars().chain(vec![BOUNDARY].into_iter()).peekable();
+    //     while let Some(next) = chars.peek() {
+    //         let prob = prob_matrix[&[stoi(cur) as usize, stoi(*next) as usize]];
+    //         let logprob = prob.ln();
+    //         log_likelihood += logprob;
+    //         n += 1;
+    //         cur = chars.next().unwrap();
+    //     }
+    // }
+    // let nll = -log_likelihood;
+    // println!("{:.4}", nll / n as f32);
 }
 
-fn stoi(c: char) -> usize {
+fn stoi(c: char) -> i32 {
     if c == BOUNDARY {
         0
     } else {
-        let i = (c as usize) - ('a' as usize) + 1;
+        let i = (c as i32) - ('a' as i32) + 1;
         if i > 26 {
             panic!()
         }
@@ -63,11 +66,27 @@ fn get_bigrams(names: &[String]) -> Matrix<f32, 27, 27> {
         let mut cur = BOUNDARY;
         let mut chars = name.chars().peekable();
         while let Some(next) = chars.peek() {
-            bigrams = bigrams.set(&[stoi(cur), stoi(*next)], |n| n + 1.0);
+            bigrams = bigrams.set(&[stoi(cur) as usize, stoi(*next) as usize], |n| n + 1.0);
             cur = chars.next().unwrap();
         }
-        bigrams = bigrams.set(&[stoi(cur), stoi(BOUNDARY)], |n| n + 1.0);
+        bigrams = bigrams.set(&[stoi(cur) as usize, stoi(BOUNDARY) as usize], |n| n + 1.0);
     }
 
     bigrams
+}
+
+fn get_training_set(names: &[String]) -> (Vec<i32>, Vec<i32>) {
+    let mut ins = Vec::new();
+    let mut outs = Vec::new();
+    for name in names {
+        let mut cur = BOUNDARY;
+        let mut chars = name.chars().chain(vec![BOUNDARY].into_iter()).peekable();
+        while let Some(next) = chars.peek() {
+            ins.push(stoi(cur));
+            outs.push(stoi(*next));
+            cur = chars.next().unwrap();
+        }
+    }
+
+    (ins, outs)
 }
