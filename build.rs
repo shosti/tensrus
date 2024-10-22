c332517ec9daff30d417241e26088eeb4f646d9e
Emanuel Evans <me@eevans.co>
2024-10-22 13:28:03 -0700

    Get blas working with linux/openblas

--- build.rs
+++ build.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("cargo:rustc-link-lib=framework=Accelerate");
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    if target_os == "macos" {
+        println!("cargo:rustc-link-lib=framework=Accelerate");
+    } else if target_os == "linux" {
+        println!("cargo:rustc-link-lib=dylib=openblas");
+    }
 }
